63bf7811f22bf2dffbfc875abf01b9d19ac5d023
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-07-18 00:47:33 +0530

    add today, version and help cmds

---
+++ cmds/help.js
@@ -0,0 +1,21 @@
+const menus = {
+  main: `
+      node-forecast [command] <options>
+  
+      today .............. show weather for today
+      version ............ show package version
+      help ............... show help menu for a command
+      `,
+
+  today: `
+      node-forecast today <options>
+  
+      --location, -l ..... the location to use
+      `,
+};
+
+module.exports = (args) => {
+  const subCmd = args._[0] === "help" ? args._[1] : args._[0];
+
+  console.log(menus[subCmd] || menus.main);
+};
